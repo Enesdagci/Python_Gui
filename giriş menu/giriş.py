8dc37939000947aa76a4ec538ed28b19b63f5ee6
Enesdagci <162743017+Enesdagci@users.noreply.github.com>
2024-12-18 18:28:39 +0300

    Updated Entry Gui
    Text labels are left side on the application screen.

--- giriş menu/giriş.py
+++ giriş menu/giriş.py
@@ -12,7 +12,7 @@ center_x = int(window_width /2 - screen_width /2)
 center_y = int(window_height /2 - screen_height/2)
 
 def login_show():
-    print("Wellcome {}{}, your password is the {}".format(name.get(),surname.get(),password.get()))
+    print("Wellcome {} {}, your password is the {}".format(name.get(),surname.get(),password.get()))
 
 screen.geometry(f"{screen_width}x{screen_height}+{center_x}+{center_y}")
 screen.title("Sign in")
@@ -23,29 +23,26 @@ text_title = tk.Label(screen, text="Hoşgeldiniz...",font=("Ariel",20))
 text_title.pack()
 
 text_message = tk.Label(screen, text="Adınız: ",font=("Ariel",14))
-text_message.pack()
+text_message.pack(anchor="w")
 
 name = tk.StringVar()
 text_name = tk.Entry(screen, textvariable=name)
 text_name.pack(fill="x")
 
 text_message_1 = tk.Label(screen,text="Soyadınız:",font=("Ariel",14))
-text_message_1.pack()
+text_message_1.pack(anchor="w")
 
 surname = tk.StringVar()
 text_surname = tk.Entry(screen, textvariable=surname)
 text_surname.pack(fill="x")
 
 text_message_2 = tk.Label(screen, text="Şifre",font=("Ariel",14))
-text_message_2.pack()
+text_message_2.pack(anchor="w")
 
 password = tk.StringVar()
-text_password = tk.Entry(screen, textvariable=password)
+text_password = tk.Entry(screen, textvariable=password,show=".")
 text_password.pack(fill="x")
 
-login = tk.Button(screen,
-                  text="Sign In",
-                  font=("Ariel",14),
-                  command=login_show)
+login = tk.Button(screen,text="Sign In",font=("Ariel",14),command=login_show)
 login.pack(ipadx = 10 )
 screen.mainloop()
