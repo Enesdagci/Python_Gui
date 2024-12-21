d5d0cca3b73058a684c3edc792d91acfd6310309
Enesdagci <162743017+Enesdagci@users.noreply.github.com>
2024-12-21 17:35:48 +0300

    Fixed İssues and Updated Versions
    Fixed unusable variables(surname),text boxes are deleted.
    Updated necessary attributes used so code is cleaned little bit.

--- giriş menu/giriş.py
+++ giriş menu/giriş.py
@@ -3,7 +3,7 @@ import tkinter as tk
 screen = tk.Tk()
 
 screen_width = 360
-screen_height = 360
+screen_height = 180
 
 window_width = screen.winfo_screenwidth()
 window_height = screen.winfo_screenheight()
@@ -12,37 +12,32 @@ center_x = int(window_width /2 - screen_width /2)
 center_y = int(window_height /2 - screen_height/2)
 
 def login_show():
-    print("Wellcome {} {}, your password is the {}".format(name.get(),surname.get(),password.get()))
+    print("Wellcome {}, your password is the {}".format(name.get(),password.get()))
 
 screen.geometry(f"{screen_width}x{screen_height}+{center_x}+{center_y}")
 screen.title("Sign in")
 screen.resizable(False,False)
 screen.iconbitmap("./pythontutorial-1.ico")
+screen.columnconfigure(0,weight=1)
+screen.columnconfigure(1,weight=3)
 
 text_title = tk.Label(screen, text="Hoşgeldiniz...",font=("Ariel",20))
-text_title.pack()
+text_title.grid(column=0,row=0,sticky="w",padx=5)
 
-text_message = tk.Label(screen, text="Adınız: ",font=("Ariel",14))
-text_message.pack(anchor="w")
+text_message = tk.Label(screen, text="Kullanıcı: ",font=("Ariel",14))
+text_message.grid(column=0,row=1,sticky="w",padx=5)
 
 name = tk.StringVar()
 text_name = tk.Entry(screen, textvariable=name)
-text_name.pack(fill="x")
-
-text_message_1 = tk.Label(screen,text="Soyadınız:",font=("Ariel",14))
-text_message_1.pack(anchor="w")
-
-surname = tk.StringVar()
-text_surname = tk.Entry(screen, textvariable=surname)
-text_surname.pack(fill="x")
+text_name.grid(column=1,row=1)
 
 text_message_2 = tk.Label(screen, text="Şifre",font=("Ariel",14))
-text_message_2.pack(anchor="w")
+text_message_2.grid(column=0,row=2,sticky="w",padx=5)
 
 password = tk.StringVar()
 text_password = tk.Entry(screen, textvariable=password,show=".")
-text_password.pack(fill="x")
+text_password.grid(column=1,row=2)
 
 login = tk.Button(screen,text="Sign In",font=("Ariel",14),command=login_show)
-login.pack(ipadx = 10 )
+login.grid(column=1,row=3)
 screen.mainloop()
